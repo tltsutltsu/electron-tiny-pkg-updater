7764acda09147c1e11955a908c985f6ac80decf4
Ilya Kalitov <tltsutltsu@mail.ru>
2022-10-20 16:53:09 +0600

    debug

--- index.js
+++ index.js
@@ -141,14 +141,14 @@ class TinyUpdater {
         fileStream.on("error", (e) => {
           this.emitter.emit("error", e);
           fileStream.destroy();
-          fs.remove(file);
+          fs.unlinkSync(file);
           reject();
         });
       });
       req.on("error", (e) => {
         this.emitter.emit("error", e);
         fileStream.destroy();
-        fs.remove(file);
+        fs.unlinkSync(file);
         reject();
       });
       req.end();
